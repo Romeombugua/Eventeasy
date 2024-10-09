079d901a11fb96f1b7d2e6e2c8f2f9020852061a
Romeombugua <romeombugua@gmail.com>
2024-10-09 18:57:16 +0300

    Static dir

--- Eventeasy/backend/settings.py
+++ Eventeasy/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -208,7 +209,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'  # URL to access the media files
 MEDIA_ROOT = BASE_DIR / 'media'
