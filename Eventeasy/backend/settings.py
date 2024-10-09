fd13a5689a98d6194f1e7dfc6265fe34a2302b1c
Romeombugua <romeombugua@gmail.com>
2024-10-09 19:49:01 +0300

    requirements.txt updated 2

--- Eventeasy/backend/settings.py
+++ Eventeasy/backend/settings.py
@@ -209,7 +209,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'  # URL to access the media files
 MEDIA_ROOT = BASE_DIR / 'media'
